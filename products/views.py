2055318a9c3962fd7941ac96111c630decf14ae5
oludare89 <azizoyekan@hotmail.com>
2021-08-22 22:32:14 +0000

    Added sorting JS and back to top link on product page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.function import Lower
+
 from .models import Product, Category
 
 # Create your views here.
