a1221986c2854763f7d1010a563eda5decaa500d
oludare89 <azizoyekan@hotmail.com>
2021-08-23 22:19:24 +0000

    added sizes to product model and size selectors in the product detail template

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
@@ -24,7 +24,7 @@ def all_products(request):
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
-                sortkey == 'category__name'
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
