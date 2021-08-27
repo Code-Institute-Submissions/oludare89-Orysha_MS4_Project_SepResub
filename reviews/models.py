154739cfc240f77fe873962cfb0b9bd9b79db8de
oludare89 <azizoyekan@hotmail.com>
2021-08-27 13:24:25 +0000

    Initial completion of webhook handler

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,3 @@
 from django.db import models
 
-
-class Review(models.Model):
-    review = models.TextField()
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+# Create your models here.
