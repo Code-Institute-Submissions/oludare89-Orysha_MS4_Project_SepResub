b76ad920182af80637c5d53e4b50b5670596a9b1
oludare89 <azizoyekan@hotmail.com>
2021-08-30 21:53:08 +0000

    Attempted to fix the wishlist views

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404, reverse, HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.db.models import Q
-from django.db.models.functions import Lower
 
-from .models import Product, Category
-from .forms import ProductForm
+
+from .models import Product,
 from wishlist.contexts import wishlist_contents
 
 # Create your views here.
@@ -13,70 +11,99 @@ from wishlist.contexts import wishlist_contents
 def show_wishlist(request):
     """ A view to show the wishlist """
     
-    return render(request, 'wishlist/wishlist.html', context)
-
+    return render(request, 'wishlist/wishlist.html')
 
 @login_required
-def add_product(request):
-    """ A view to add products to the site """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only the site owner can do that.')
-        return redirect(reverse('home'))
+def add_to_wishlist(request, item_id):
+    """ Add a quantity of the specified product to the wishlist """
 
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save()
-            messages.success(request, 'Successfully added product!')
-            return redirect(reverse('product_detail', args=[product.id]))
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    wishlist = request.session.get('wishlist', {})
+
+    if size:
+        if item_id in list(wishlist.keys()):
+            if size in wishlist[item_id]['items_by_size'].keys():
+                wishlist[item_id]['items_by_size'][size] += quantity
+                messages.success(request, f"Updated size {size.upper()} {product.name} quantity to {wishlist[item_id]['items_by_size'][size]}")
+            else:
+                wishlist[item_id]['items_by_size'][size] = quantity
+                messages.success(request, f'Added size {size.upper()} {product.name} to your wishlist') 
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid')
+            wishlist[item_id] = {'items_by_size': {size: quantity}}
+            messages.success(request, f'Added size {size.upper()} {product.name} to your wishlist') 
     else:
-        form = ProductForm()
-        
-    template = 'products/add_product.html'
-    context = {
-        'form': form,
-    }
-
-    return render(request, template, context)
-
-
-@login_required
-def edit_product(request, product_id):
-    """ To edit an existing product on the site """
-    if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only the site owner can do that.')
-        return redirect(reverse('home'))
-
-    product = get_object_or_404(Product, pk=product_id)
-    if request.method == 'POST':
-        form = ProductForm(request.POST, request.FILES, instance=product)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Successfully updated product!")
-            return redirect(reverse('product_detail', args=[product.id]))
+        if item_id in list(wishlist.keys()):
+            wishlist[item_id] += quantity
+            messages.success(request, f'Updated {product.name} quantity to {wishlist[item_id]}')
         else:
-            messages.error(request, "Failed to update product. Please ensure form is valid.")
-    else:
-        form = ProductForm(instance=product)
-        messages.info(request, f'You are editing {product.name}')
+            wishlist[item_id] = quantity
+            messages.success(request, f'Added {product.name} to your cart')
 
-    template = 'products/edit_product.html'
-    context = {
-        'form': form,
-        'product': product,
-    }
-
-    return render(request, template, context)
+    request.session['wishlist'] = wishlist
+    return redirect(redirect_url)
 
 
 @login_required
-def delete_wishlist_item(request, product_id):
-    """ To delete an existing wishlist item """
-    
-        
-    product = get_object_or_404(Product, pk=product_id)
-    wishlist_item.delete()
-    messages.success(request, 'Wishlist item deleted!')
-    return redirect(reverse('wishlist'))
+def adjust_wishlist(request, item_id):
+    """ Adjust the quantity of the specified product by the specified amount """
+
+    product = get_object_or_404(Product, pk=item_id)
+    quantity = int(request.POST.get('quantity'))
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
+    wishlist = request.session.get('wishlist', {})
+
+    if size:
+        if quantity > 0:
+            wishlist[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f"Updated size {size.upper()} {product.name} quantity to {wishlist[item_id]['items_by_size'][size]}")
+        else:
+            del wishlist[item_id]['items_by_size'][size]
+            if not wishlist[item_id]['items_by_size']:
+                wishlist.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your wishlist')
+    else:
+        if quantity > 0:
+            wishlist[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {wishlist[item_id]}')
+        else:
+            wishlist.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your wishlist')
+
+
+    request.session['wishlist'] = wishlist
+    return redirect(reverse('show_wishlist'))
+
+
+@login_required
+def remove_from_wishlist(request, item_id):
+    """ Remove the specified product from the wishlist """
+
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        wishlist = request.session.get('wishlist', {})
+
+        if size:
+            del wishlist[item_id]['items_by_size'][size]
+            if not wishlist[item_id]['items_by_size']:
+                wishlist.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your wishlist')
+        else:
+            wishlist.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your wishlist')
+
+        request.session['wishlist'] = wishlist
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
