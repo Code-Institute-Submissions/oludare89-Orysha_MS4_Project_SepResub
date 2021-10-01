5fadf51aadd4cea8b5b1fdece899d2cc2390bae1
oludare89 <azizoyekan@hotmail.com>
2021-10-01 08:52:31 +0000

    Removed AWS Object Parameters

--- orysha/settings.py
+++ orysha/settings.py
@@ -197,11 +197,6 @@ DEFAULT_FROM_EMAIL = 'orysha@example.com'
 
 if 'USE_AWS' in os.environ:
 
-    AWS_S3_OBJECT_PARAMETERS = {
-        'Expires': 'Thur 31 Dec 2099 20:00:00 GMT',
-        'CacheControl': 'max-age=94608000'
-    }
-
     AWS_STORAGE_BUCKET_NAME = 'orysha-app'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
